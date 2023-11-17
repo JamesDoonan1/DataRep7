caa36086e34e2079130b3e86210e36ed1e306cd9
James <g00310428@atu.ie>
2023-11-17 10:44:40 +0000

    first commit

--- src/components/books.js
+++ src/components/books.js
@@ -4,7 +4,8 @@ function Books(props){
 
     return props.myBooks.map(
         (book)=>{
-            return <BookItem myBook={book} key={book.isbn}></BookItem>
+            // _id is taking directly from MongoDB  e.g "_id": "65573766b6a9e922682e83f7"
+            return <BookItem myBook={book} key={book._id}></BookItem>
         }
     );
 
