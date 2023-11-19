c46657156215d9bfb305513e0a0e8b3c40e3d459
James Doonan <124049432+JamesDoonan1@users.noreply.github.com>
2023-11-19 19:42:40 +0000

    Update books.js

--- src/components/books.js
+++ src/components/books.js
@@ -1,8 +1,10 @@
+// Importing the BookItem component from the specified file path
 import BookItem from "./bookItem";
 
 function Books(props){
 
     return props.myBooks.map(
+        // Arrow function that takes each book as a parameter
         (book)=>{
             // _id is taking directly from MongoDB  e.g "_id": "65573766b6a9e922682e83f7"
             return <BookItem myBook={book} key={book._id}></BookItem>
@@ -11,4 +13,5 @@ function Books(props){
 
 }
 
+// Exporting the Books component as the default export of this module
 export default Books;
